769bb8e38af1cf049d36c9df29fe1a8ccc91ce42
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-09-04 10:24:37 +0530

    created the server and posted data on server

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
-//create a server object:
-//createServer takes fucntion as a paramater and serves server on port 8080.
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'application\json'});
+    res.write(JSON.stringify({name:'Pavan', 'email':'pavan@gmail.com'}))
+    res.end();
+}).listen(8080)
 
+// https://www.w3schools.com/nodejs/nodejs_http.asp
 
-http.createServer()
+// Response 200 - OK (while doing GET request)
+//          201 - Created (while doing POST request)
+//          500 - Internal Server error
+//          404 - Page not found
