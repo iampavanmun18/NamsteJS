4a2230e2a808b3cbd30957f841bba644948a5c08
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-09-05 10:06:46 +0530

    async-task don't wait for anyone they will execute in their order

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// Sync-tasks -  they will wait for first task then 2nd and then third. e.g PHP
 console.log("Start Here!!")
 
 console.log("Middle Here!!")
 
 console.log("End Here!!")
+
+//Async-task -  they don't wait for anyone. e.g Node, Javascript
+console.log("1st Task")
+
+setTimeout(() => {
+   console.log("I am 2nd task")
+}, 2000);
+
+
+console.log("3rd task")
