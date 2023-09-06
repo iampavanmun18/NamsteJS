c02f7c53e38c2459446930611b0d347acb027256
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-09-06 18:06:03 +0530

    middlware (reqFilter) added to routing as 2nd parameter

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// display html data on web page
-app.get('/',(req,res)=>{
-   res.send("<h1>Hello This is Home Page</h1>")
+const reqFilter = (req, res, next) => {
+  if (!req.query.age) {
+    res.send("Please Provide the age");
+  } else if (req.query.age < 18) {
+    res.send("Your Age is Less than 18 you can't Login to Home Page");
+  } else if (req.query.age) {
+    res.send("This is a Home Page");
+  } else {
+    next();
+  }
+};
+
+//we can use middlwware using (app.use) and we can pass this to routes as a 2nd Param below.
+app.use(reqFilter);
+
+//reqFilter is a middleware used in routing below
+app.get("/", reqFilter, (req, res) => {
+  res.json([
+    {
+      name: "Pavan",
+      email: "pavan@gmail.com",
+    },
+    {
+      name: "Yash",
+      email: "yash@gmail.com",
+    },
+  ]);
 });
 
-//req -> req.query (req) will come from frontend.
-app.get('/about',(req,res)=>{
-   console.log("About", req.query.name)
-   res.send("Welcome to About Page,"+ req.query.name)
-})
-
-// display input data on web page
-app.get('/help',(req,res)=>{
-res.send("<input type='text' placeholder ='username'/>")
-})
-
-// display json data on web page
-app.get('/details',(req,res)=>{
-   res.send({
-      name:'Pavan',
-      email:'Pavan@gmail.com'
-   })
-   })
-
-app.listen(4000,()=>{
-   console.log("server is running")
-})
+app.listen(4000);
